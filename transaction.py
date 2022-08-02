3a3061fda60174dd5fd05816b1717cd1c5b05f29
YoanaCt <leidy.caro@udea.edu.co>
2022-08-01 21:34:23 -0500

    Refactor transaction module

--- transaction.py
+++ transaction.py
@@ -1,4 +1,4 @@
-from blockchain import *
+from hash import *
 
 coins = 50000000
 reward = 20
@@ -9,21 +9,49 @@ wallets = {
 	'3': 0,
 	'4': 0,
 	'5': 0
-}
+	}
 
-walletsBefore = wallets.copy()
-
-def transaction(wallet1, wallet2, send):
-	if wallet1 < send:
-		return 'ERROR'
+	
+def transaction(wallet1, wallet2, send):	
+	walletsBefore = wallets.copy()
+	
+	if walletsBefore[wallet1] < send:
+		print('ERROR')
+		return
 	else:
-		wallet1 -= send
-		wallet2 += send
+		wallets[wallet1] -= send
+		wallets[wallet2] += send	
 
-	wallets[wallet1] = wallet1
-	wallets[wallet2] = wallet2
+	transaction = f'Estado antes de la transacción: {walletsBefore}, Estado luego de la transacción: {wallets} '  
+	print(transaction)	
+	return transaction
 
-	tx = f'Estado antes de la transacción: {walletsBefore}, Estado luego de la transacción: {wallets} '  
-	return tx
+def getTransactions():
+	transactions = ''
+	txList = []
+	while True:
+		transactions = input('Hacer transacción: y/n\n').lower()
 
+		if transactions == 'y':
+			walletOrigin = input('ingrese la wallet origen\n')
+			walletDestiny = input('ingrese la wallet destino\n')
+			send = int(input('Ingrese el monto a enviar\n'))
+		
+			try:				
+				tx = transaction(walletOrigin, walletDestiny, send)
+				txList.append(hasheo(tx))
+			except Exception as e:
+				print(f'Ocurrió un error: {e}, ingrese valores correctos ')				
+			finally:
+				print(txList)
+		elif transactions == 'n':
+			break
+		else:
+			print('Opción inválida')
+			break
+	return txList
+
+
+if __name__ == '__main__':	
+	transaction(wallets[1],wallets[2],50)
 
